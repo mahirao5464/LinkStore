96fc2b7fb84d7885ffd02610a2e38752f5685952
Mahipal <Mahirao5464@gmail.com>
2021-11-23 13:54:04 +0530

    prerenderer changes

--- LinkStore/Server/Program.cs
+++ LinkStore/Server/Program.cs
@@ -1,11 +1,22 @@
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.AspNetCore.ResponseCompression;
-
+using MudBlazor.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddMudServices();
+builder.Services.AddSingleton<HttpClient>(sp =>
+{
+    // Get the address that the app is currently running at
+    var server = sp.GetRequiredService<IServer>();
+    var addressFeature = server.Features.Get<IServerAddressesFeature>();
+    string baseAddress = addressFeature.Addresses.First();
+    return new HttpClient { BaseAddress = new Uri(baseAddress) };
+});
 
 var app = builder.Build();
 
@@ -29,8 +40,15 @@ app.UseStaticFiles();
 app.UseRouting();
 
 
-app.MapRazorPages();
-app.MapControllers();
-app.MapFallbackToFile("index.html");
+//app.MapRazorPages();
+//app.MapControllers();
+//app.MapFallbackToFile("index.html");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapRazorPages();
+    endpoints.MapControllers();
+    endpoints.MapFallbackToPage("/_Host");
+});
+
 
 app.Run();
